5515cbb483e494393b4068371b6d5fe64ecf1365
Doug Youch <doug@sessionm.com>
2013-06-22 01:25:39 -0400

    added a ranged connection pool, basically for a given key connect to the correct set of servers

---
+++ lib/with_connection/ranged_connection_pool.rb
@@ -0,0 +1,48 @@
+module WithConnection
+  class RangedConnectionPool
+    attr_reader :list, :default_pool, :key_algo
+
+    def initialize(ranges_and_pools, default_pool, key_algo)
+      @list = ranges_and_pools.map { |range, pool| Item.new(range, pool) }
+      @default_pool = default_pool
+      @key_algo = key_algo
+    end
+
+    def with_connection(key=nil, &block)
+      pool_for_key(key).with_connection(&block)
+    end
+
+    def pool_for_key(key)
+      key = self.key_algo.call(key) if key
+      key.nil? ?
+      @default_pool : 
+        (@list.detect { |item| item.include?(key) } || @default_pool)
+    end
+
+    class Item
+      attr_reader :range, :pool
+
+      def initialize(range, pool)
+        @range = range
+        @pool = pool
+      end
+
+      def include?(key)
+        @range.include? key
+      end
+    end
+
+    class BasicRange
+      attr_reader :lo, :hi
+
+      def initialize(lo, hi)
+        @lo = lo
+        @hi = hi
+      end
+
+      def include?(val)
+        val >= lo && val <= hi
+      end
+    end
+  end
+end
